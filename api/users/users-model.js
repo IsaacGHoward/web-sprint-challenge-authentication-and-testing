a7af776c3b5fe0fe4d4f5b6a6434999ad1b5d4ca
IsaacGHoward <howardisaac100@gmail.com>
2021-03-21 15:56:36 -0700

    Added cookieparser and some other imports

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,10 +1,24 @@
-const db = require('../../data/db-config.js');
+const db = require('../../data/dbConfig');
 
 function find() {
   return db('users')
     .select('*')
 }
 
+function findById(user_id) {
+  return db('users as u')
+    .where('u.id', user_id)
+    .first('u.id', 'u.username')
+}
+
+function findByName(username) {
+  return db('users as u')
+    .where('u.username', username)
+    .first('u.id', 'u.username')
+}
+
 module.exports = {
   find,
+  findById,
+  findByName
 };
